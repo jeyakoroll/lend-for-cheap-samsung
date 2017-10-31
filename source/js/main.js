409b7c227ea89086e1443cb3551d9491cbba2535
Jenya Korolenko <jeyakoroll@gmail.com>
2017-10-31 11:34:44 +0200

    Site colors and buy are done

--- source/js/main.js
+++ source/js/main.js
@@ -8,10 +8,25 @@ $(document).ready(function() {
   $( '.header__bottom-list_form select[name=size]' ).addClass( 'footer__form-size' );
   $( '.header__bottom-list_form .order-form' ).addClass( 'footer__form-order' );
   $( '.header__bottom-list_form input' ).addClass( 'footer__form-data' );
-  $( '.header__bottom-list_form button' ).addClass( 'footer__form-button' );
+  var 
+      buttonInForm = $( '.header__bottom-list_form button' ),
+      parentThisButton = buttonInForm.parent();
+
+      buttonInForm.addClass( 'footer__form-button' );
+      parentThisButton.addClass( 'footer__form-button_wrap' );
+      
   $( '.header__bottom-list_form input[name=color]' ).addClass( 'footer__form-checkbox' );
   $('.header__bottom-list_form input[name=name_first]').attr("placeholder","Имя");
   $('.header__bottom-list_form input[name=name_last]').attr("placeholder","Телефон");
+
+  // change the text in the button
+  $(window).on('load resize', function() {
+    var screenWidth = $(window).width(),
+        contentButton = $('.footer__form-button'),
+        isPhone = screenWidth < 480;
+        
+    contentButton = isPhone ? contentButton.empty().text('Заказать') : contentButton.empty().text('Заказать по акции');
+  })
 
 });
 
