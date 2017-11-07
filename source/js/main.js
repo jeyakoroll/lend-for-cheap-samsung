f30b42a35f50a754cc057e7591dbe5ddeb52b0e9
Jenya Korolenko <jeyakoroll@gmail.com>
2017-11-07 10:41:22 +0200

    almoust done

--- source/js/main.js
+++ source/js/main.js
@@ -104,20 +104,20 @@ $(document).ready(function() {
 
 
   // timer
-  // (function() {
-  //   $(document).ready(function() {
-  //     var now = new Date(),
-  //       secPassed = now.getHours() * 60 * 60 + now.getMinutes() * 60 + now.getSeconds();
-  //     var t = (60 * 60 * 24) - secPassed;
-  //     $('.timer').countdown({
-  //       until: (t),
-  //       labels: ['Годы', 'Месяцы', 'Недели', 'Дни', 'Часов', 'Минут', 'Секунд'],
-  //       labels1: ['Годы', 'Месяцы', 'Недели', 'Дни', 'Часов', 'Минут', 'Секунд'],
-  //       format: 'HMS',
-  //       layout: '<li><div><span>{h10}</span></div><div><span>{h1}</span></div></li><li><div><span>{m10}</span></div><div><span>{m1}</span></div></li><li><div><span>{s10}</span></div><div><span>{s1}</span></div></li>'
-  //     });
-  //   });
-  // })();
+  (function() {
+    $(document).ready(function() {
+      var now = new Date(),
+        secPassed = now.getHours() * 60 * 60 + now.getMinutes() * 60 + now.getSeconds();
+      var t = (60 * 60 * 24) - secPassed;
+      $('.timer').countdown({
+        until: (t),
+        labels: ['Годы', 'Месяцы', 'Недели', 'Дни', 'Часов', 'Минут', 'Секунд'],
+        labels1: ['Годы', 'Месяцы', 'Недели', 'Дни', 'Часов', 'Минут', 'Секунд'],
+        format: 'HMS',
+        layout: '<li><div><span>{h10}</span></div><div><span>{h1}</span></div></li><li><div><span>{m10}</span></div><div><span>{m1}</span></div></li><li><div><span>{s10}</span></div><div><span>{s1}</span></div></li>'
+      });
+    });
+  })();
 
 
   // checkbox in top form
